6e0d8d6d1aa5c443377702555cd3439c512c492e
Arun <arun21june@gmail.com>
2021-02-15 03:07:50 +0530

    Commit test Java dn TrackCoach

---
+++ com/luv2code/springdemo/Test.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test class for push test");
+
+	}
+
+}
--- com/luv2code/springdemo/TrackCoach.java
+++ com/luv2code/springdemo/TrackCoach.java
@@ -14,8 +14,3 @@ public class TrackCoach implements Coach {
 
 
 
-
-
-
-
-
