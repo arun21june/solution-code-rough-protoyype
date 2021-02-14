cba296c20ae56574c7546b08cd73a929044d9331
Arun <arun21june@gmail.com>
2021-02-15 03:54:43 +0530

    New change for github

--- com/luv2code/springdemo/Test.java
+++ com/luv2code/springdemo/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a test class for push test");
+		System.out.println("This is a test class for push test -1");
+		
 
 	}
 
